46c3beaf59c8295fb8868d75add035ad3788e28a
LeanderStern <rednael3@freenet.de>
2025-10-06 01:08:48 +0200

    Remove logging setup and logger property from PydanticBaseModel

--- base_model.py
+++ base_model.py
@@ -1,25 +1,5 @@
-import logging
-from functools import cached_property
-from logging import Logger
-from typing import ClassVar
-
 from pydantic import BaseModel, ConfigDict
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler()
-    ]
-)
 
 class PydanticBaseModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True,
                               strict=True)
-    _LOGGER: ClassVar[Logger | None] = None
-
-    @cached_property
-    def logger(self) -> Logger:
-        if self._LOGGER is None:
-            self.__class__._LOGGER = logging.getLogger(f"{self.__class__.__module__}.{self.__class__.__name__}")
-        return self.__class__._LOGGER
